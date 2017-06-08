480875170474daa9bf70aaa4c556a678f08e07d0
unnivm <unnivm@gmail.com>
2017-06-08 09:01:45 +0530

    java file

--- SOS.java
+++ SOS.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class SOS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Scanner in = new Scanner(System.in);
 		String S = in.next();
